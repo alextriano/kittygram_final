95fcdcb801c53361f5112b00f0a263e0672c94cd
Александр Волков <alextriano@gmail.com>
2023-12-03 19:33:23 +0300

    Add fix20

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'key')
 
-DEBUG = bool(strtobool(os.getenv('DEBUG', 'false')))
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['158.160.82.27', '127.0.0.1', 'localhost', 'alextrianohost.ddnsking.com']
 
